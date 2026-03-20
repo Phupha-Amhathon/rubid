1a4eeae63f56faee24f80bfb9ecf921cc2e2fc9f
Puwadon Puchamni <puwadon.p@kkumail.com>
2026-03-21 02:18:29 +0700

    Update and rename machine_rotate_7.vhd to machine_cut.vhd

--- rubid.srcs/sources_1/new/machine_cut.vhd
+++ rubid.srcs/sources_1/new/machine_cut.vhd
@@ -22,14 +22,29 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity machine_rotate_7 is
+entity machine_cut is
     Port ( d_in  : in  STD_LOGIC_VECTOR(15 downto 0);
            d_out : out STD_LOGIC_VECTOR(15 downto 0));
-end machine_rotate_7;
+end machine_cut;
 
-architecture Structural of machine_rotate_7 is
+architecture GateLevel of machine_cut is
 begin
-    -- Shift left by 7. 
-    -- Bits 8 down to 0 shift up, Bits 15 down to 9 wrap around to the bottom.
-    d_out <= d_in(8 downto 0) & d_in(15 downto 9);
-end Structural;
+    --8-0 go above
+    d_out(15) <= d_in(8);
+    d_out(14) <= d_in(7);
+    d_out(13) <= d_in(6);
+    d_out(12) <= d_in(5);
+    d_out(11) <= d_in(4);
+    d_out(10) <= d_in(3);
+    d_out(9)  <= d_in(2);
+    d_out(8)  <= d_in(1);
+    d_out(7)  <= d_in(0);
+    --15-9 go below
+    d_out(6)  <= d_in(15);
+    d_out(5)  <= d_in(14);
+    d_out(4)  <= d_in(13);
+    d_out(3)  <= d_in(12);
+    d_out(2)  <= d_in(11);
+    d_out(1)  <= d_in(10);
+    d_out(0)  <= d_in(9);
+end GateLevel;
